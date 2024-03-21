47de711d6e8eb3b53beaba0703f301ee4ed8db63
dguajardo <diego.guajardo@hotmail.cl>
2024-03-21 12:21:31 -0300

    feat: logic to craps

--- craps/src/app/app.component.ts
+++ craps/src/app/app.component.ts
@@ -9,5 +9,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'craps';
+  leftCrap = '../assets/img/dice1.png';
+  rightCrap = '../assets/img/dice2.png';
+
+  play1: number = 0;
+  play2: number = 0;
+
+  public handlePLay() {
+    this.play1 = Math.floor(Math.random() * 6) + 1;
+    this.play2 = Math.floor(Math.random() * 6) + 1;
+    this.leftCrap = '../assets/img/dice' + this.play1 + '.png';
+    this.rightCrap = '../assets/img/dice' + this.play2 + '.png';
+  }
+
 }
